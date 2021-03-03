23cdb7b3d913c9a73d0b3238296c3a95818cb532
Joshua J Arnold <joshua@blackswanfarming.com>
2021-03-04 07:59:51 +1300

    changed to Juliette

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ const AboutPage = () => (
     <main> 
         <title> About! </title>
         <h1>Who are you?</h1>
-        <p>My name is Joshua. I like surfing and building stuff</p>
+        <p>My name is Juliette. I like dancing and learning stuff</p>
 
         <Link to="/">Back to Home!</Link>
         
