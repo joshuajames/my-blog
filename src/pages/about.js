1eec1b200648ca6f665d8c03d2a99711463f8949
Joshua J Arnold <joshua@blackswanfarming.com>
2021-03-04 07:31:21 +1300

    Link to Home

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,15 @@
 // about page
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => (
     <main> 
         <title> About! </title>
         <h1>Who are you?</h1>
         <p>My name is Joshua. I like surfing and building stuff</p>
+
+        <Link to="/">Back to Home!</Link>
+        
     </main>
 )
 
