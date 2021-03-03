1c303b334e3861a6ca2dacc95bb95ebf9e2fb9a9
Joshua J Arnold <joshua@blackswanfarming.com>
2021-03-04 07:18:34 +1300

    About

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+// about page
+import React from 'react'
+
+const AboutPage = () => (
+    <main> 
+        <title> About! </title>
+        <h1>Who are you?</h1>
+        <p>My name is Joshua. I like surfing and building stuff</p>
+    </main>
+)
+
+export default AboutPage
